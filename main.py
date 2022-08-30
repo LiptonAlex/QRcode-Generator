368fd867449ef2eb990e8566e5dbf19009068fd9
Alex Lipton <99798521+LiptonAlex@users.noreply.github.com>
2022-08-30 17:49:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyqrcode
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+
+#representing the QR code string wich cases your url
+string = 'https://www.instagram.com/mylnitsa.shop/'
+#creating te QR
+url = pyqrcode.create(string)
+#QRcode saveng
+url.svg('myQR.svg', scale = 8)
+#converting SVG to PNG format and saving
+drawing = svg2rlg('myQR.svg')
+renderPM.drawToFile(drawing, 'MyQRcode.png', fmt = 'PNG')
